76a5342e9b7f8d7d93b22614772fb0352de0dd4c
AtumaDavid <atumadavid35@gmail.com>
2023-02-08 12:25:48 +0100

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,18 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
 
 function buildStyles() {
   // return src("index.scss").pipe(sass()).pipe(dest("css"));
-  return src("sass-folder/**/*.scss").pipe(sass()).pipe(dest("css"));
+  return src("sass-folder/**/*.scss")
+    .pipe(sass())
+    .pipe(purgecss({ content: ["*.html"] }))
+    .pipe(dest("css"));
 }
 
 function watchTask() {
   // watch(["index.scss"], buildStyles);
-  watch(["sass-folder/**/*.scss"], buildStyles);
+  watch(["sass-folder/**/*.scss", "*.html"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
